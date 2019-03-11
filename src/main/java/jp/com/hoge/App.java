10023b854170653085eb88d9417341e8658bf5eb
KamataHiromichi <1216kamata@gmail.com>
2019-03-12 06:09:04 +0900

    complete

--- src/main/java/jp/com/hoge/App.java
+++ src/main/java/jp/com/hoge/App.java
@@ -3,6 +3,7 @@ package jp.com.hoge;
 import java.io.*;
 import java.net.URLEncoder;
 import java.net.HttpURLConnection;
+import java.net.ProtocolException;
 import java.net.URL;
 import java.nio.charset.Charset;
 import java.lang.StringBuilder;
@@ -17,8 +18,16 @@ import com.amazonaws.services.lambda.runtime.*;
 
 public class App implements RequestStreamHandler
 {
+    public static String SLACK_BOT_USER_ACCESS_TOKEN = "";
+    public static String SLACK_APP_AUTH_TOKEN = "";
+    public static String USER_ID = "";
+
     @Override
     public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
+        App.SLACK_BOT_USER_ACCESS_TOKEN = System.getenv("SLACK_BOT_USER_ACCESS_TOKEN");
+        App.SLACK_APP_AUTH_TOKEN = System.getenv("SLACK_APP_AUTH_TOKEN");
+        App.USER_ID = System.getenv("USER_ID");
+
         // デフォルトのresponse 何も返信しないと何度か同じイベントが送られてくる
         String response = "HTTP 200 OK\nContent-type: text/plain\nOK";
         try{
@@ -26,6 +35,8 @@ public class App implements RequestStreamHandler
             String jsonText = readAll(rd);
             System.out.println(jsonText); //System.outの出力はCloudWatchLogsに書き込まれる
             JSONObject json = new JSONObject(jsonText);
+
+            // Event APIのテストの時
             if (json.has("type")) {
                 String eventType = json.get("type").toString();
                 if (eventType.equals("url_verification")) {
@@ -33,6 +44,23 @@ public class App implements RequestStreamHandler
                     response = "HTTP 200 OK\nContent-type: text/plain\n" + json.get("challenge").toString();
                 }
             }
+            
+            // app_mentionイベントの時
+            if (json.has("event")) {
+                JSONObject eventObject = json.getJSONObject("event");
+                if(eventObject.has("type")) {
+                    String eventType = eventObject.get("type").toString();
+                    if (eventType.equals("app_mention")){
+                        String user = eventObject.get("user").toString();
+                        if (user.equals(App.USER_ID)) { return; } // 発言がbot user自身の場合は無視する
+                        String channel = eventObject.get("channel").toString();
+                        String text = eventObject.get("text").toString();
+                        String responseText = text.replace(App.USER_ID, user);
+                        System.out.println(responseText);
+                        System.out.println(postMessage(responseText, channel));
+                    }
+                }
+            }       
         } catch(IOException e) {
             e.printStackTrace();
         } finally {
@@ -52,5 +80,82 @@ public class App implements RequestStreamHandler
             sb.append((char) cp);
         }
         return sb.toString();
+    }
+
+    /* post message to selected channel */
+    public static String postMessage(String message, String channel) {
+        String strUrl = "https://slack.com/api/chat.postMessage";
+        String ret = "";
+        URL url;
+
+        HttpURLConnection urlConnection = null;
+        try {
+            url = new URL(strUrl);
+            urlConnection = (HttpURLConnection) url.openConnection();
+        } catch(IOException e) {
+            e.printStackTrace();
+            return "IOException";
+        }
+        
+        urlConnection.setDoOutput(true);
+        urlConnection.setConnectTimeout(100000);
+        urlConnection.setReadTimeout(100000);
+        urlConnection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+        String auth = "Bearer " + App.SLACK_BOT_USER_ACCESS_TOKEN;
+        urlConnection.setRequestProperty("Authorization", auth);
+
+        try {
+            urlConnection.setRequestMethod("POST");
+        } catch(ProtocolException e) {
+            e.printStackTrace();
+            return "ProtocolException";
+        }
+
+        try {
+            urlConnection.connect();
+        } catch(IOException e) {
+            e.printStackTrace();
+            return "IOException";
+        }
+
+        HashMap<String, Object> jsonMap = new HashMap<>();
+        jsonMap.put("token", App.SLACK_APP_AUTH_TOKEN);
+        jsonMap.put("channel", channel);
+        jsonMap.put("text", message);
+        jsonMap.put("username", " sampleBot");
+
+        OutputStream outputStream = null;
+        try {
+            outputStream = urlConnection.getOutputStream();
+        } catch(IOException e) {
+            e.printStackTrace();
+            return "IOException";
+        }
+
+        if (jsonMap.size() > 0) {
+            JSONObject responseJsonObject = new JSONObject(jsonMap);
+            String jsonText = responseJsonObject.toString();
+            PrintStream ps = new PrintStream(outputStream);
+            ps.print(jsonText);
+            ps.close();
+        }
+
+        try {
+            if (outputStream != null) {
+                outputStream.close();
+            }
+            int responseCode = urlConnection.getResponseCode();
+            BufferedReader rd = new BufferedReader(new InputStreamReader(urlConnection.getInputStream(), "UTF-8"));
+            ret = readAll(rd);
+        } catch(IOException e) {
+            e.printStackTrace();
+            return "IOException";
+        
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }         
+        }
+        return ret;
     }
 }
